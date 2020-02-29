705cf8e291a67d71ef2516eaebddec7d65031396
merves <kymerve16@gmail.com>
2020-02-28 21:55:48 -0800

    Added eda function with documentation and fixed indentation.

--- nursepy/nursepy.py
+++ nursepy/nursepy.py
@@ -1,12 +1,65 @@
 import numpy as np
 import pandas as pd
+import altair as alt
+from collections import defaultdict
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder, StandardScaler, RobustScaler
 from sklearn.compose import ColumnTransformer
 
 
-def preproc(X_train, X_test=None, auto=False, OHE=[], standard_scale=[], robust_scale=[], numerical_impute=[], categegorical_impute=[], label_encode={}):
+def eda(input_data):
+    """
+    Generates a dictionary to access histogram and summary statistics for each column in a given data frame.
+
+    Parameters
+    ----------
+    input_data: pandas.DataFrame
+        input dataframe to be analyzed
+
+    Returns
+    -------
+    dict
+        a dictionary that contains a histogram and summary statistics for each column.
+
+    Examples
+    --------
+    >>> from nursepy import eda
+    >>> np.random.seed(0)
+    >>> input_data = pd.DataFrame(np.random.randn(10, 1))
+    >>> input_data.columns = ['my_attribute']
+    >>> results = eda(input_data)
+    >>> results
+    {'histograms': {'my_attribute': alt.Chart(...)},
+             'stats': {'my_attribute': my_attribute
+              count  10.000000
+              mean    0.861331
+              std     0.681438
+              min    -0.103219
+              25%     0.345767
+              50%     0.855563
+              75%     1.365244
+              max     1.867558}}
+    """
+
+    eda_summary = defaultdict(dict)
+
+    # for each column, calculate altair histogram and descriptive statistics
+    for column_name in input_data.columns:
+        column_specific_hist = alt.Chart(input_data).mark_bar().encode(
+            alt.X(column_name + ":Q", bin=True),
+            y='count()'
+        )
+        eda_summary["histograms"][column_name] = column_specific_hist
+
+        column_specific_stats = pd.DataFrame(input_data[column_name].describe())
+        eda_summary["stats"][column_name] = column_specific_stats
+
+    return eda_summary
+
+
+def preproc(X_train, X_test=None, auto=False, OHE=[], standard_scale=[], robust_scale=[], numerical_impute=[],
+            categegorical_impute=[], label_encode={}):
     """
     Preprocesses data frames, including onehot encoding, scaling, and imputation, and label encoding
 
@@ -35,7 +88,7 @@ def preproc(X_train, X_test=None, auto=False, OHE=[], standard_scale=[], robust_
     # automatically choose which columns to scale and encode
     if (auto == True):
         for element in [OHE, standard_scale, robust_scale, numerical_impute, categegorical_impute]:
-            if(len(element) > 0):
+            if (len(element) > 0):
                 raise Exception(
                     f'You cannot manually set {element} when setting auto to true')
         label_keys = label_encode.keys()
@@ -50,24 +103,24 @@ def preproc(X_train, X_test=None, auto=False, OHE=[], standard_scale=[], robust_
         transformer = ColumnTransformer(
             transformers=[
                 ('cat_imputer',
-                    SimpleImputer(strategy='constant', fill_value='missing'),
-                    categegorical_impute
+                 SimpleImputer(strategy='constant', fill_value='missing'),
+                 categegorical_impute
                  ),
                 ('num_imputer',
-                    SimpleImputer(strategy='median'),
-                    numerical_impute
+                 SimpleImputer(strategy='median'),
+                 numerical_impute
                  ),
                 ("one_hot",
-                    OneHotEncoder(drop='first'),
-                    OHE
+                 OneHotEncoder(drop='first'),
+                 OHE
                  ),
                 ("standard_scalar",
-                    StandardScaler(),
-                    standard_scale
+                 StandardScaler(),
+                 standard_scale
                  ),
                 ("robust_scalar",
-                    RobustScaler(),
-                    robust_scale
+                 RobustScaler(),
+                 robust_scale
                  )
             ],
             remainder='passthrough'  # donot apply anything to the remaining columns
@@ -76,24 +129,24 @@ def preproc(X_train, X_test=None, auto=False, OHE=[], standard_scale=[], robust_
         transformer = ColumnTransformer(
             transformers=[
                 ('cat_imputer',
-                    SimpleImputer(strategy='constant', fill_value='missing'),
-                    categegorical_impute
+                 SimpleImputer(strategy='constant', fill_value='missing'),
+                 categegorical_impute
                  ),
                 ('num_imputer',
-                    SimpleImputer(strategy='median'),
-                    numerical_impute
+                 SimpleImputer(strategy='median'),
+                 numerical_impute
                  ),
                 ("one_hot",
-                    OneHotEncoder(),
-                    OHE
+                 OneHotEncoder(),
+                 OHE
                  ),
                 ("standard_scalar",
-                    StandardScaler(),
-                    standard_scale
+                 StandardScaler(),
+                 standard_scale
                  ),
                 ("robust_scalar",
-                    RobustScaler(),
-                    robust_scale
+                 RobustScaler(),
+                 robust_scale
                  )
             ],
             remainder='passthrough'  # donot apply anything to the remaining columns
